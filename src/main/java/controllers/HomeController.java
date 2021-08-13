464419b26c59678a4d4d2a2ce1371214ddb272a9
toannvph05233 <johntoan98@gmail.com>
2021-08-13 15:49:04 +0700

    upp bai 3

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,49 +1,26 @@
 package controllers;
 
-import models.Customer;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import service.CRUDCustomer;
 
-import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
     CRUDCustomer crudCustomer = new CRUDCustomer();
 
     @RequestMapping("/show")
-    public String home(HttpServletRequest request) {
-        request.setAttribute("list", crudCustomer.list);
-        return "/WEB-INF/show.jsp";
+    public ModelAndView home() {
+        ModelAndView modelAndView = new ModelAndView("show");
+        modelAndView.addObject("list", crudCustomer.list);
+        return modelAndView;
     }
-
-    @GetMapping("/edit")
-    public String showedit(HttpServletRequest request, @RequestParam int index) {
-        request.setAttribute("customer", crudCustomer.list.get(index));
-        return "/WEB-INF/edit.jsp";
-    }
-
     @GetMapping("/create")
-    public String showCreate() {
-        return "/WEB-INF/create.jsp";
-    }
-
-    @GetMapping("/delete")
-    public String delete(@RequestParam int index) {
-        crudCustomer.delete(index);
-        return "redirect:/show";
+    public ModelAndView create(){
+        ModelAndView modelAndView = new ModelAndView("create");
+        return modelAndView;
     }
 
 
-    @PostMapping("/edit")
-    public String edit(@RequestParam int index,@ModelAttribute Customer customer) {
-        crudCustomer.edit(customer, index);
-        return "redirect:/show";
-    }
-
-    @PostMapping("/create")
-    public String creaate(@ModelAttribute Customer customer) {
-        crudCustomer.save(customer);
-        return "redirect:/show";
-    }
 }
